4b92be67087872848a392d5a0159ee82136fe8e4
Meghan Denny <hello@nektro.net>
2025-08-22 02:25:22 -0700

    fix title

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ export default {
 						<meta charset="UTF-8">
 						<meta name="viewport" content="width=device-width, initial-scale=1.0">
 						<meta http-equiv="X-UA-Compatible" content="ie=edge">
-						<title>Meghan Denny</title>
+						<title>⌛ ${url}</title>
 						<meta http-equiv="refresh" content="1; url='${url}'" />
 					</head>
 					<body>
